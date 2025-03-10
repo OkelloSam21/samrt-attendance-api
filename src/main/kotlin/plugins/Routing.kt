8ad5d13205ee2509a551012585765462687ca311
okello <samuelokello929@gmail.com>
2025-03-10 19:10:48 +0300

    Add Swagger UI and refactor route initialization
    Integrated Swagger UI for API documentation, pointing to the OpenAPI file. Refactored routing by grouping and importing all route definitions using wildcard imports. This enhances maintainability and provides better API visibility.

--- src/main/kotlin/plugins/Routing.kt
+++ src/main/kotlin/plugins/Routing.kt
@@ -1,15 +1,17 @@
 package plugins
 
 import io.ktor.server.application.*
-import io.ktor.server.response.*
+import io.ktor.server.plugins.swagger.*
 import io.ktor.server.routing.*
-import routes.assignmentRoutes
-import routes.attendanceRoutes
-import routes.courseRoutes
-import routes.userRoutes
+import routes.*
+
 
 fun Application.configureRouting() {
-    routing {
+       routing {
+        swaggerUI(path = "swagger", swaggerFile = "openapi/documentation.yaml") {
+            version = "2.0"
+        }
+        authRoutes()
         userRoutes()
         assignmentRoutes()
         attendanceRoutes()
