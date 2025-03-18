ba6aef5b694a22db0e8ae3b052429b4e0fa9fad3
okello <samuelokello929@gmail.com>
2025-03-18 11:55:55 +0300

    enhancement  - create separate table for Student   to ease querying of data and to hold student records

--- src/main/kotlin/models/Users.kt
+++ src/main/kotlin/models/Users.kt
@@ -1,23 +1,33 @@
 package models
 
 import org.jetbrains.exposed.dao.id.UUIDTable
+import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.javatime.CurrentTimestamp
 import org.jetbrains.exposed.sql.javatime.timestamp
+
+
+// UserRole Enum
+enum class UserRole {
+    STUDENT, LECTURER, ADMIN
+}
 
 object Users : UUIDTable("users") {
     val name = varchar("name", 255)
     val email = varchar("email", 255).uniqueIndex()
-    val regNo = varchar("reg_no", 255).nullable().uniqueIndex()
-    val employeeRoleNo = varchar("employee_role_no", 255).nullable().uniqueIndex()
     val password = text("password") // Store hashed passwords here
     val role = enumerationByName("role", 10, UserRole::class)
     val createdAt = timestamp("created_at").defaultExpression(CurrentTimestamp())
     val updatedAt = timestamp("updated_at").nullable()
 }
 
-// UserRole Enum
-enum class UserRole {
-    STUDENT, LECTURER, ADMIN
+object Students : Table("students") {
+    val userId = reference("user_id", Users.id)
+    val regNo = varchar("reg_no", 255).uniqueIndex()
+    val department = varchar("department", 100).nullable()
+    val yearOfStudy = integer("year_of_study").nullable()
+
+    override val primaryKey = PrimaryKey(userId)
+
 }
 
 object Staff : Table("staff") {
