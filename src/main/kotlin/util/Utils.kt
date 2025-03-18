0a0519b074521166b823de8344281fb90fa76ac7
okello <samuelokello929@gmail.com>
2025-03-18 11:57:20 +0300

    enhancement  - get configuration from AppConfig

--- src/main/kotlin/util/Utils.kt
+++ src/main/kotlin/util/Utils.kt
@@ -1,19 +1,11 @@
 package util
 
-import auth.JwtConfig
-import io.ktor.server.application.Application
+// Reuse the configuration loaded via AppConfig
+val SECRET: String get() = AppConfig.jwtSecret
+val ISSUER: String get() = AppConfig.jwtIssuer
+val AUDIENCE: String get() = AppConfig.jwtAudience
 
-// Lazy initialization for configuration to ensure loadJwtConfig runs properly
-private lateinit var jwtConfig: JwtConfig
-
-fun initializeJwtConfig(application: Application) {
-    jwtConfig = application.loadJwtConfig()
-}
-
-// Reuse the configuration loaded via loadJwtConfig
-val SECRET: String get() = jwtConfig.secret
-val ISSUER: String get() = jwtConfig.issuer
-val AUDIENCE: String get() = jwtConfig.audience
+//val PORT: String get() = jwtConfig.port
 
 const val ACCESS_TOKEN_EXPIRATION = 15 * 60 * 1000 // 15 minutes
 const val REFRESH_TOKEN_EXPIRATION = 7 * 24 * 60 * 60 * 1000 // 7 days
