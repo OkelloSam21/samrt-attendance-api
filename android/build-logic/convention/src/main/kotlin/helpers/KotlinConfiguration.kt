77c0811d14d9f549b75043176ad2909fdb25889d
okello <samuelokello929@gmail.com>
2025-03-22 23:10:52 +0300

    feat: add android build-logic
    - Create `settings.gradle.kts` for build-logic configuration.
    - Create `libs.versions.toml` to manage dependencies versions.
    - Create `KotlinConfiguration.kt` and `LibraryConventionPlugin.kt` to centralize and apply configuration logic.
    - Add kotlin configuration for Android.
    - Add library convention plugin.

---
+++ android/build-logic/convention/src/main/kotlin/helpers/KotlinConfiguration.kt
@@ -0,0 +1,64 @@
+package helpers
+
+import AndroidSdk
+import com.android.build.api.dsl.CommonExtension
+import extensions.compilerOptions
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+
+//internal fun Project.configureKotlinAndroid(
+//    extension: CommonExtension<*, *, *, *, *,*>,
+//) {
+//    extension.apply {
+//        compileSdk = AndroidSdk.compileSdk
+//        defaultConfig {
+//            minSdk = AndroidSdk.minimumSdk
+//        }
+//        compileOptions {
+//            sourceCompatibility = JavaVersion.VERSION_17
+//            targetCompatibility = JavaVersion.VERSION_17
+//        }
+//        buildFeatures {
+//            buildConfig = true
+//        }
+//    }
+//    extensions. {
+//            allWarningsAsErrors.set(true)
+//            freeCompilerArgs.addAll(
+//                "-opt-in=kotlin.RequiresOptIn",
+//                "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+//                "-opt-in=kotlinx.coroutines.FlowPreview",
+//                "-opt-in=kotlin.Experimental"
+//            )
+//            jvmTarget.set(JvmTarget.fromTarget(JavaVersion.VERSION_17.toString()))
+//        }
+//    }
+//}
+
+internal fun Project.configureKotlinAndroid(
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
+) {
+    commonExtension.apply {
+        compileSdk = AndroidSdk.compileSdk
+        defaultConfig {
+            minSdk = AndroidSdk.minimumSdk
+        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
+        }
+        buildFeatures {
+            buildConfig = true
+        }
+    }
+    commonExtension.compilerOptions {
+        allWarningsAsErrors = true
+        freeCompilerArgs = freeCompilerArgs + listOf(
+            "-opt-in=kotlin.RequiresOptIn",
+            "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+            "-opt-in=kotlinx.coroutines.FlowPreview",
+            "-opt-in=kotlin.Experimental"
+        )
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
