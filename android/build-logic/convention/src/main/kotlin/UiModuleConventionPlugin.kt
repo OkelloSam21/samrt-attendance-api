ed875da9cafa3b86216d586a7f18f2e1298c25de
okello <samuelokello929@gmail.com>
2025-03-25 00:01:52 +0300

    feat: add navigation and hilt dependencies to `UiModuleConventionPlugin`
    -   Add `androidx-navigation`, `hilt-android`, and `hilt-android-compiler` dependencies to the `UiModuleConventionPlugin`.
    -   Apply `com.google.devtools.ksp` and `com.google.dagger.hilt.android` plugins in `UiModuleConventionPlugin`.

--- android/build-logic/convention/src/main/kotlin/UiModuleConventionPlugin.kt
+++ android/build-logic/convention/src/main/kotlin/UiModuleConventionPlugin.kt
@@ -1,16 +1,19 @@
-import AndroidSdk
 import com.android.build.gradle.LibraryExtension
 import extensions.configureAndroidCompose
 import helpers.configureKotlinAndroid
+import helpers.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 
 class UiModuleConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
         pluginManager.apply("com.android.library")
         pluginManager.apply("org.jetbrains.kotlin.android")
         pluginManager.apply("org.jetbrains.kotlin.plugin.compose")
+        pluginManager.apply("com.google.devtools.ksp")
+        pluginManager.apply("com.google.dagger.hilt.android")
 
         extensions.configure<LibraryExtension> {
             configureKotlinAndroid(this)
@@ -18,5 +21,12 @@ class UiModuleConventionPlugin : Plugin<Project> {
         }
 
         configureAndroidCompose(extensions.getByType(LibraryExtension::class.java))
+
+        dependencies{
+            "implementation"(libs.findLibrary("androidx-navigation").get())
+
+            "implementation"(libs.findLibrary("hilt-android").get())
+            "ksp"(libs.findLibrary("hilt-android-compiler").get())
+        }
     }
 }
