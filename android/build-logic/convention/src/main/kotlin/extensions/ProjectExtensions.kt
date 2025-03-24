454938373e949dd76176bd4af5f70a6efe49c6a9
okello <samuelokello929@gmail.com>
2025-03-25 00:21:31 +0300

    feat: configure android compose options and dependencies
    -   Configures Android Compose within the `CommonExtension`.
    -   Sets `compose` to true in `buildFeatures`.
    -   Sets `kotlinCompilerExtensionVersion` in `composeOptions` from version catalog.
    -   Adds `compose-activity` as an implementation dependency.
    -   Adds `compose-bom` as `platform` for dependencies.
    -   Adds `compose` bundle, `compose-ui-tooling`, and `compose-ui-test-manifest` dependencies.

--- android/build-logic/convention/src/main/kotlin/extensions/ProjectExtensions.kt
+++ android/build-logic/convention/src/main/kotlin/extensions/ProjectExtensions.kt
@@ -3,18 +3,15 @@ package extensions
 import com.android.build.api.dsl.ApplicationExtension
 import com.android.build.api.dsl.CommonExtension
 import com.android.build.gradle.LibraryExtension
+import helpers.libs
 import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.api.plugins.PluginManager
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.getByType
 
 internal fun Project.configureAndroidCompose(
     commonExtension: CommonExtension<*, *, *, *, *, *>,
 ) {
-
-    val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
 
     commonExtension.apply {
 
@@ -22,8 +19,8 @@ internal fun Project.configureAndroidCompose(
             compose = true
         }
 
-        compilerOptions {
-            freeCompilerArgs
+        composeOptions {
+            kotlinCompilerExtensionVersion = libs.findVersion("compose-compiler").get().toString()
         }
 
         dependencies {
@@ -31,6 +28,7 @@ internal fun Project.configureAndroidCompose(
             val bom = libs.findLibrary("compose-bom").get()
             add("implementation", platform(bom))
             add("implementation", libs.findBundle("compose").get())
+            add("implementation", libs.findLibrary("compose-activity").get())
             add("androidTestImplementation", platform(bom))
             add("debugImplementation", libs.findLibrary("compose-ui-tooling").get())
             add("debugImplementation", libs.findLibrary("compose-ui-test-manifest").get())
