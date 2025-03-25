2c99755add591981b788b71f0a8c3c20046d476f
okello <samuelokello929@gmail.com>
2025-03-25 14:05:02 +0300

    Initialize MainNavHost and navigation setup
    This commit introduces the `MainNavHost` composable, which serves as the central navigation point for the application using `NavHost` from `androidx.navigation.compose`.
    It also removes the previous placeholder UI elements like `Greeting` and related code, updating the `MainActivity`'s `setContent` to prepare for the new navigation setup.
    
    Specifically, the changes include:
    
    -   Creation of `MainNavHost.kt` to manage app navigation.
    -   Implementation of `MainNavHost` composable, integrating `NavHost` with a placeholder start destination.
    - Removal of the `Greeting` composable and preview from `MainActivity.kt`.
    - Updating the `MainActivity.kt` to prepare for the implementation of the `MainNavHost` composable
    - Installing `SplashScreen` in `MainActivity.kt`

---
+++ android/modules-ui/presentation/src/main/kotlin/com/smartattendance/presentation/navigation/MainNavHost.kt
@@ -0,0 +1,15 @@
+package com.smartattendance.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+
+@Composable
+fun MainNavHost(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = ""
+    ) {
+
+    }
+}
