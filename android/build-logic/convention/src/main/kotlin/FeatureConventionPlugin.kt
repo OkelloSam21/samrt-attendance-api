523ea07f99e44b30d5c1291eb058cea51649d636
OkelloSam <samuelokello929@gmail.com>
2025-03-27 11:11:23 +0300

    Refactor: Rename `FeatureModuleConventionPlugin` to `FeatureConventionPlugin`
    This commit renames the `FeatureModuleConventionPlugin` class and its corresponding file to `FeatureConventionPlugin`. This change aims to improve clarity and consistency in the naming of convention plugins within the project.

--- android/build-logic/convention/src/main/kotlin/FeatureConventionPlugin.kt
+++ android/build-logic/convention/src/main/kotlin/FeatureConventionPlugin.kt
@@ -7,7 +7,7 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 
-class FeatureModuleConventionPlugin : Plugin<Project> {
+class FeatureConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
         pluginManager.apply("com.android.library")
         pluginManager.apply("com.google.devtools.ksp")
