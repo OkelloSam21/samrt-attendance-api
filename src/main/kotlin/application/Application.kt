4a22a1d9dc389e2c782641b918d62c185b4c924e
SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
2025-04-14 13:04:36 +0300

    Create Application.kt with Ktor module function
    Create standard Ktor module entry point to fix deployment issue.
    The module function consolidates all plugin configurations and routing
    that were previously called directly in Main.kt.

---
+++ src/main/kotlin/application/Application.kt
@@ -0,0 +1,16 @@
+package application
+
+import io.ktor.server.application.*
+import plugins.*
+
+fun Application.module() {
+    // Install plugins
+    configureContentNegotiation()
+    configureStatusPages()
+    configureAuthentication()
+    configureCors()
+    configureSwagger()
+
+    // Register routes
+    configureRouting()
+}
