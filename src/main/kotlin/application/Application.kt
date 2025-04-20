ded684f5efa0c449419df04976ed0405c65f9b4d
SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
2025-04-20 00:48:10 +0000

    update

--- src/main/kotlin/application/Application.kt
+++ src/main/kotlin/application/Application.kt
@@ -9,23 +9,6 @@ import plugins.*
 
 private val logger = KotlinLogging.logger {}
 
-fun main() {
-    try {
-        AppConfig.logConfiguration()
-
-        // Start the server
-        logger.info { "Starting HTTP server on ${AppConfig.server.host}:${AppConfig.server.port}" }
-        embeddedServer(
-            factory = Netty,
-            port = AppConfig.server.port,
-            host = AppConfig.server.host
-        ) {
-            module()
-        }.start(wait = true)
-    } catch (e: Exception) {
-        logger.error(e) { "Failed to start application: ${e.message}" }
-    }
-}
 
 fun Application.module() {
     // Install plugins
