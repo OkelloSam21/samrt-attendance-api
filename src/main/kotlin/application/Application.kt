515e64685b19fe350c3b22f0ad8dd893ee243329
OkelloSam <samuelokello929@gmail.com>
2025-04-20 06:52:23 +0300

    update

--- src/main/kotlin/application/Application.kt
+++ src/main/kotlin/application/Application.kt
@@ -1,13 +1,7 @@
 package application
 
-import config.AppConfig
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import mu.KotlinLogging
 import plugins.*
-
-private val logger = KotlinLogging.logger {}
 
 
 fun Application.module() {
--- src/main/kotlin/application/Main.kt
+++ src/main/kotlin/application/Main.kt
@@ -1,10 +1,9 @@
-import application.module
+package application
+
 import config.AppConfig
-import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import mu.KotlinLogging
-import plugins.*
 
 private val logger = KotlinLogging.logger {}
 
